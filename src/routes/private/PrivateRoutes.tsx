fa051627017ac13c1abdbee1c06307ca0e83a141
Biswopaban Nayak <biswopabannayak@gmail.com>
2023-06-06 23:21:57 +0530

    Changes Done

--- src/routes/private/PrivateRoutes.tsx
+++ src/routes/private/PrivateRoutes.tsx
@@ -1,15 +1,21 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StyleSheet } from 'react-native'
 import React from 'react'
+import Story from '../../screen/private/story/Story'
+import Comments from '../../screen/private/Comments'
 import { Home } from '../../screen/private'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Bottomtab from '../layout/Bottomtab'
 
 const PrivateRoutes = () => {
   const Stack = createNativeStackNavigator()
   return (
     <Stack.Navigator
-      initialRouteName='Home'
+      initialRouteName='Bottomtab'
       screenOptions={{ headerShown: false }}
     >
+      <Stack.Screen name='Bottomtab' component={Bottomtab} />
+      <Stack.Screen name='Comments' component={Comments} />
+      <Stack.Screen name='Story' component={Story} />
       <Stack.Screen name='Home' component={Home} />
 
     </Stack.Navigator>
