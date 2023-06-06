697122bb90c123a6e73709cdb191a9be1eb42a1c
Biswopaban Nayak <biswopabannayak@gmail.com>
2023-06-06 23:22:12 +0530

    Changes Done

---
+++ src/screen/private/Comments.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+import React from 'react'
+import { Box } from 'native-base'
+
+const Comments = () => {
+    return (
+        <Box
+            bgColor={'#fff'}
+            flex={1}
+        >
+
+        </Box>
+    )
+}
+
+export default Comments
+
+const styles = StyleSheet.create({})
