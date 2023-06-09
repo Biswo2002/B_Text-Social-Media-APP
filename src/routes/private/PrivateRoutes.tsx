bd6121a4537c4dd56655ebe7b95cd47f3b4ce487
Biswopaban Nayak <biswopabannayak@gmail.com>
2023-06-10 03:36:05 +0530

    Complete Charting SCreen

--- src/routes/private/PrivateRoutes.tsx
+++ src/routes/private/PrivateRoutes.tsx
@@ -1,8 +1,12 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StyleSheet } from 'react-native'
 import React from 'react'
-import Story from '../../screen/private/story/Story'
+import RequestMessage from '../../screen/private/RequestMessage'
+import CallScreen from '../../screen/private/CallScreen'
+import VideoCall from '../../screen/private/VideoCall'
 import Comments from '../../screen/private/Comments'
+import Story from '../../screen/private/story/Story'
+import Message from '../../screen/private/Message'
 import Inbox from '../../screen/private/Inbox'
 import { Home } from '../../screen/private'
 import Bottomtab from '../layout/Bottomtab'
@@ -14,8 +18,12 @@ const PrivateRoutes = () => {
       initialRouteName='Bottomtab'
       screenOptions={{ headerShown: false }}
     >
+      <Stack.Screen name='RequestMessage' component={RequestMessage} />
+      <Stack.Screen name='CallScreen' component={CallScreen} />
       <Stack.Screen name='Bottomtab' component={Bottomtab} />
+      <Stack.Screen name='VideoCall' component={VideoCall} />
       <Stack.Screen name='Comments' component={Comments} />
+      <Stack.Screen name='Message' component={Message} />
       <Stack.Screen name='Inbox' component={Inbox} />
       <Stack.Screen name='Story' component={Story} />
       <Stack.Screen name='Home' component={Home} />
