152ba6474f99f840a04983017afbcdd845542e6c
Biswopaban Nayak <biswopabannayak@gmail.com>
2023-06-11 01:11:40 +0530

    Complete all Screen

--- src/screen/private/Search.tsx
+++ src/screen/private/Search.tsx
@@ -1,11 +1,235 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import { StyleSheet, Dimensions } from 'react-native'
+import React, { useState } from 'react'
+import {
+    Pressable,
+    FlatList,
+    Icon,
+    Image,
+    Input,
+    Row,
+    Box,
+    Text,
+    ScrollView
+} from 'native-base'
+import { COLORS, FONTS } from '../../styles'
 
+const data = [
+    {
+        id: '1',
+        title: 'Trending',
+    },
+    {
+        id: '2',
+        title: 'Discover',
+    },
+    {
+        id: '3',
+        title: 'Post',
+    },
+    {
+        id: '4',
+        title: 'Shorts',
+    },
+]
+const data1 = [
+    {
+        id: '1',
+        img: 'https://img.freepik.com/free-photo/girl-red-dress-reaches-out-hand-with-cookie-her-father_8353-952.jpg?t=st=1686378257~exp=1686378857~hmac=058ac91e86482be01f3d71b51507b69071be3924a1bd433d4a67b283a55a6a19',
+    },
+    {
+        id: '2',
+        img: 'https://img.freepik.com/free-photo/small-boy-with-car-sitting-bed_8353-382.jpg?w=360&t=st=1686412839~exp=1686413439~hmac=993dc19a283dd47fc355743386d7f3ea292b7e3967bb7f17274aa05d11c89464',
+    },
+    {
+        id: '3',
+        img: 'https://img.freepik.com/free-photo/smiling-romantic-asian-girl-contemplating-nature-around_1262-19410.jpg?t=st=1686412483~exp=1686413083~hmac=35c8f367e05de1ca035785c9974da6454d20a09b192baeb613c4613d17fb445f',
+    },
+    {
+        id: '4',
+        img: 'https://img.freepik.com/free-photo/close-up-brunette-woman-with-colorful-jersey_1140-286.jpg?t=st=1686378257~exp=1686378857~hmac=352e66f7478ea86c20e21b1a2a865af5fb5b94d983b8f0f7a9cd0a99b283995f',
+    },
+    {
+        id: '5',
+        img: 'https://img.freepik.com/free-photo/attractive-young-woman-with-ceremonial-make-up_23-2148187417.jpg?t=st=1686412483~exp=1686413083~hmac=cf67a22175e0a16daebba82b1b6422c4e266daee6c51677efeefd176a91a2cd5',
+    },
+    {
+        id: '6',
+        img: 'https://img.freepik.com/free-photo/portrait-beautiful-young-woman-with-hands-hip-front-colonnade_23-2148187353.jpg',
+    },
+    {
+        id: '7',
+        img: 'https://img.freepik.com/free-photo/attractive-young-woman-with-ceremonial-make-up_23-2148187417.jpg?t=st=1686412483~exp=1686413083~hmac=cf67a22175e0a16daebba82b1b6422c4e266daee6c51677efeefd176a91a2cd5',
+    },
+    {
+        id: '8',
+        img: 'https://img.freepik.com/free-photo/portrait-attractive-caucasian-couple-red-smiling-camera-blurred-xmas-wreath-background_132075-6461.jpg?t=st=1686378257~exp=1686378857~hmac=109f689e6edd23ae4ceb74a06510739eaccfc77db63aa65aa12360ce0f719fc6',
+    },
+    {
+        id: '9',
+        img: 'https://img.freepik.com/free-photo/small-boy-with-car-sitting-bed_8353-382.jpg?w=360&t=st=1686412839~exp=1686413439~hmac=993dc19a283dd47fc355743386d7f3ea292b7e3967bb7f17274aa05d11c89464',
+    },
+    {
+        id: '10',
+        img: 'https://img.freepik.com/free-photo/day-two-green-cold-happiness_1157-2530.jpg?t=st=1686412599~exp=1686413199~hmac=5a76b43d62eff2d64462e072c53a6726d4db74016b09e281988fbafeb3874308',
+    },
+    {
+        id: '11',
+        img: 'https://img.freepik.com/free-photo/pretty-girl-with-long-dark-hair-laughing-winter-outdoor-shot-romantic-caucasian-woman-chilling-cold-day_197531-14102.jpg?t=st=1686412661~exp=1686413261~hmac=c93bac34c5f40a483af2ac902ee7e1ccd2e99e481530aa27eeb67f4b30be6608',
+    },
+    {
+        id: '12',
+        img: 'https://img.freepik.com/free-photo/girl-red-dress-reaches-out-hand-with-cookie-her-father_8353-952.jpg?t=st=1686378257~exp=1686378857~hmac=058ac91e86482be01f3d71b51507b69071be3924a1bd433d4a67b283a55a6a19',
+    },
+    {
+        id: '13',
+        img: 'https://img.freepik.com/free-photo/day-two-green-cold-happiness_1157-2530.jpg?t=st=1686412599~exp=1686413199~hmac=5a76b43d62eff2d64462e072c53a6726d4db74016b09e281988fbafeb3874308',
+    },
+    {
+        id: '14',
+        img: 'https://img.freepik.com/free-photo/smiling-romantic-asian-girl-contemplating-nature-around_1262-19410.jpg?t=st=1686412483~exp=1686413083~hmac=35c8f367e05de1ca035785c9974da6454d20a09b192baeb613c4613d17fb445f',
+    },
+    {
+        id: '15',
+        img: 'https://img.freepik.com/free-photo/girl-red-dress-reaches-out-hand-with-cookie-her-father_8353-952.jpg?t=st=1686378257~exp=1686378857~hmac=058ac91e86482be01f3d71b51507b69071be3924a1bd433d4a67b283a55a6a19',
+    },
+    {
+        id: '16',
+        img: 'https://img.freepik.com/free-photo/smiling-romantic-asian-girl-contemplating-nature-around_1262-19410.jpg?t=st=1686412483~exp=1686413083~hmac=35c8f367e05de1ca035785c9974da6454d20a09b192baeb613c4613d17fb445f',
+    },
+    {
+        id: '17',
+        img: 'https://img.freepik.com/free-photo/positive-red-haired-woman-laughing-blur-nature-refined-ginger-girl-smiling-during-winter-photoshoot_197531-14103.jpg?t=st=1686412741~exp=1686413341~hmac=1bce95b98d7407b2f86c2e332b1d43ff40ef8fd26516cf6ed286894b942b37ca',
+    },
+    {
+        id: '18',
+        img: 'https://img.freepik.com/free-photo/girl-red-dress-reaches-out-hand-with-cookie-her-father_8353-952.jpg?t=st=1686378257~exp=1686378857~hmac=058ac91e86482be01f3d71b51507b69071be3924a1bd433d4a67b283a55a6a19',
+    },
+    {
+        id: '19',
+        img: 'https://img.freepik.com/free-photo/smiling-romantic-asian-girl-contemplating-nature-around_1262-19410.jpg?t=st=1686412483~exp=1686413083~hmac=35c8f367e05de1ca035785c9974da6454d20a09b192baeb613c4613d17fb445f',
+    },
+    {
+        id: '20',
+        img: 'https://img.freepik.com/free-photo/amazing-red-haired-woman-with-phone-standing-street-cute-ginger-lady-coat-hat-holding-smartphone_197531-14148.jpg?t=st=1686412697~exp=1686413297~hmac=4937ea3184893af7882540b4510b8cb40101c735afb0262e304d03e930a349f7',
+    },
+    {
+        id: '21',
+        img: 'https://img.freepik.com/free-photo/girl-red-dress-reaches-out-hand-with-cookie-her-father_8353-952.jpg?t=st=1686378257~exp=1686378857~hmac=058ac91e86482be01f3d71b51507b69071be3924a1bd433d4a67b283a55a6a19',
+    },
+    {
+        id: '22',
+        img: 'https://img.freepik.com/free-photo/pretty-girl-with-long-dark-hair-laughing-winter-outdoor-shot-romantic-caucasian-woman-chilling-cold-day_197531-14102.jpg?t=st=1686412661~exp=1686413261~hmac=c93bac34c5f40a483af2ac902ee7e1ccd2e99e481530aa27eeb67f4b30be6608',
+    },
+    {
+        id: '23',
+        img: 'https://img.freepik.com/free-photo/girl-red-dress-reaches-out-hand-with-cookie-her-father_8353-952.jpg?t=st=1686378257~exp=1686378857~hmac=058ac91e86482be01f3d71b51507b69071be3924a1bd433d4a67b283a55a6a19',
+    },
+    {
+        id: '24',
+        img: 'https://img.freepik.com/free-photo/pretty-girl-with-long-dark-hair-laughing-winter-outdoor-shot-romantic-caucasian-woman-chilling-cold-day_197531-14102.jpg?t=st=1686412661~exp=1686413261~hmac=c93bac34c5f40a483af2ac902ee7e1ccd2e99e481530aa27eeb67f4b30be6608',
+    },
+]
 const Search = () => {
+    const [selectBottom, setSelectBottom] = useState('');
     return (
-        <View>
-            <Text>Search</Text>
-        </View>
+        <Box bgColor={'#fff'} flex={1}>
+            <Box
+                m={4}
+                mt={6}
+            >
+                <Input
+                    placeholder="Search Message "
+                    width="100%"
+                    focusOutlineColor={COLORS.PRIMARY}
+                    borderRadius="12"
+                    borderColor={'transparent'}
+                    backgroundColor={'gray.100'}
+                    shadow={1}
+                    py="2"
+                    px="1"
+                    // mb={'5'}
+                    fontSize="14"
+                    InputLeftElement={
+                        <Icon
+                            m="2"
+                            ml="3"
+                            size="6"
+                            color="gray.400"
+                            as={<MaterialIcons name="search" />}
+                        />
+                    }
+                />
+            </Box>
+            <Box
+                m={2}
+            >
+                <FlatList
+                    data={data}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={({ item }) => (
+                        <Pressable
+                            bgColor={
+                                selectBottom === item?.id ? COLORS.PRIMARY : COLORS.SECONDARY
+                            }
+                            shadow={2}
+                            onPress={() => setSelectBottom(item?.id)}
+                            p={2}
+                            alignItems={'center'}
+                            justifyContent={'center'}
+                            borderRadius={'30'}
+                            m={2}
+                            borderWidth={selectBottom === (item?.id) ? 0 : 2}
+                            borderColor={selectBottom === (item?.id) ? COLORS.SECONDARY : COLORS.PRIMARY}
+                        >
+                            <Text
+                                mx={4}
+                                fontSize={'sm'}
+                                color={
+                                    selectBottom === item?.id
+                                        ? COLORS.SECONDARY
+                                        : COLORS.PRIMARY
+                                }
+                                fontFamily={FONTS[600].normal}>
+                                {item?.title}
+                            </Text>
+                        </Pressable>
+                    )}
+                />
+            </Box>
+            <ScrollView
+                contentContainerStyle={{
+                    paddingBottom: '20%'
+                }}
+            >
+                <Row
+                    flexWrap={'wrap'}
+                    m={3}
+                >
+                    {
+                        data1?.map(item => (
+                            <Image
+                                key={item?.id}
+                                alt={item?.img}
+                                m={1}
+                                source={{ uri: item?.img }}
+                                width={Dimensions.get('window').width / 3.4}
+                                height={Dimensions.get('window').height / 6}
+                                // resizeMode='contain'
+                                borderRadius={'10'}
+                            />
+                        ))
+                    }
+                </Row>
+
+
+
+
+
+
+            </ScrollView>
+        </Box>
     )
 }
 
