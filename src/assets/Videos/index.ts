5f96303ffb9729db3a1867ec3feef7d6f0276ca4
Biswopaban Nayak <biswopabannayak@gmail.com>
2023-06-25 18:34:03 +0530

    Add Video

---
+++ src/assets/Videos/index.ts
@@ -0,0 +1,8 @@
+export default {
+  TWO: require('./Video2.mp4'),
+  THREE: require('./Video3.mp4'),
+  FOUR: require('./Video4.mp4'),
+  SIX: require('./Video6.mp4'),
+  SEVEN: require('./Video7.mp4'),
+  EIGHT: require('./Video8.mp4'),
+};
